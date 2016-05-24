0465792bb0ac349f7da07feed75c7f2e49bd45b0
Vincent Dumoulin <vi.dumoulin@gmail.com>
2016-05-24 13:55:40 -0400

    Add experiment scripts

---
+++ experiments/ali_celeba.py
@@ -0,0 +1,182 @@
+import argparse
+import logging
+
+from blocks.algorithms import Adam
+from blocks.bricks import LeakyRectifier, Logistic
+from blocks.bricks.conv import ConvolutionalSequence
+from blocks.extensions import FinishAfter, Timing, Printing, ProgressBar
+from blocks.extensions.monitoring import DataStreamMonitoring
+from blocks.extensions.saveload import Checkpoint
+from blocks.filter import VariableFilter
+from blocks.graph import ComputationGraph, apply_dropout
+from blocks.graph.bn import (batch_normalization,
+                             get_batch_normalization_updates)
+from blocks.initialization import IsotropicGaussian, Constant
+from blocks.main_loop import MainLoop
+from blocks.model import Model
+from blocks.roles import INPUT
+from theano import tensor
+
+from ali.algorithms import ali_algorithm
+from ali.bricks import ALI
+from ali.streams import create_celeba_data_streams
+from ali.utils import get_log_odds, conv_brick, conv_transpose_brick, bn_brick
+
+BATCH_SIZE = 100
+MONITORING_BATCH_SIZE = 500
+NUM_EPOCHS = 1000
+IMAGE_SIZE = (32, 32)
+NUM_CHANNELS = 3
+NLAT = 256
+GAUSSIAN_INIT = IsotropicGaussian(std=0.01)
+ZERO_INIT = Constant(0)
+LEARNING_RATE = 1e-4
+BETA1 = 0.5
+
+
+def create_model_brick():
+    layers = [
+        conv_brick(5, 1, 32), bn_brick(), LeakyRectifier(),
+        conv_brick(4, 2, 64), bn_brick(), LeakyRectifier(),
+        conv_brick(4, 1, 128), bn_brick(), LeakyRectifier(),
+        conv_brick(4, 2, 256), bn_brick(), LeakyRectifier(),
+        conv_brick(4, 1, 512), bn_brick(), LeakyRectifier(),
+        conv_brick(1, 1, 512), bn_brick(), LeakyRectifier(),
+        conv_brick(1, 1, 2 * NLAT)]
+    encoder = ConvolutionalSequence(
+        layers=layers, num_channels=NUM_CHANNELS, image_size=IMAGE_SIZE,
+        use_bias=False, name='encoder')
+
+    layers = [
+        conv_transpose_brick(4, 1, 256), bn_brick(), LeakyRectifier(),
+        conv_transpose_brick(4, 2, 128), bn_brick(), LeakyRectifier(),
+        conv_transpose_brick(4, 1, 64), bn_brick(), LeakyRectifier(),
+        conv_transpose_brick(4, 2, 32), bn_brick(), LeakyRectifier(),
+        conv_transpose_brick(5, 1, 32), bn_brick(), LeakyRectifier(),
+        conv_transpose_brick(1, 1, 32), bn_brick(), LeakyRectifier(),
+        conv_brick(1, 1, NUM_CHANNELS), Logistic()]
+    decoder = ConvolutionalSequence(
+        layers=layers, num_channels=NLAT, image_size=(1, 1), use_bias=False,
+        name='decoder')
+
+    layers = [
+        conv_brick(5, 1, 32), LeakyRectifier(),
+        conv_brick(4, 2, 64), bn_brick(), LeakyRectifier(),
+        conv_brick(4, 1, 128), bn_brick(), LeakyRectifier(),
+        conv_brick(4, 2, 256), bn_brick(), LeakyRectifier(),
+        conv_brick(4, 1, 512), bn_brick(), LeakyRectifier()]
+    x_discriminator = ConvolutionalSequence(
+        layers=layers, num_channels=NUM_CHANNELS, image_size=IMAGE_SIZE,
+        use_bias=False, name='x_discriminator')
+    x_discriminator.push_allocation_config()
+
+    layers = [
+        conv_brick(1, 1, 512), LeakyRectifier(),
+        conv_brick(1, 1, 512), LeakyRectifier()]
+    z_discriminator = ConvolutionalSequence(
+        layers=layers, num_channels=NLAT, image_size=(1, 1), use_bias=False,
+        name='z_discriminator')
+    z_discriminator.push_allocation_config()
+
+    layers = [
+        conv_brick(1, 1, 1024), LeakyRectifier(),
+        conv_brick(1, 1, 1024), LeakyRectifier(),
+        conv_brick(1, 1, 1)]
+    joint_discriminator = ConvolutionalSequence(
+        layers=layers,
+        num_channels=(x_discriminator.get_dim('output')[0] +
+                      z_discriminator.get_dim('output')[0]),
+        image_size=(1, 1),
+        name='joint_discriminator')
+
+    ali = ALI(encoder, decoder, x_discriminator, z_discriminator,
+              joint_discriminator, weights_init=GAUSSIAN_INIT,
+              biases_init=ZERO_INIT, name='ali')
+    ali.push_allocation_config()
+    encoder.layers[-1].use_bias = True
+    encoder.layers[-1].tied_biases = False
+    decoder.layers[-2].use_bias = True
+    decoder.layers[-2].tied_biases = False
+    x_discriminator.layers[0].use_bias = True
+    x_discriminator.layers[0].tied_biases = True
+    ali.initialize()
+    raw_marginals, = next(
+        create_celeba_data_streams(500, 500)[0].get_epoch_iterator())
+    b_value = get_log_odds(raw_marginals)
+    decoder.layers[-2].b.set_value(b_value)
+
+    return ali
+
+
+def create_models():
+    ali = create_model_brick()
+    x = tensor.tensor4('features')
+    z = ali.theano_rng.normal(
+        size=(x.shape[0],) + ali.decoder.get_dim('input_'))
+
+    def _create_model(with_dropout):
+        cg = ComputationGraph(ali.compute_losses(x, z))
+        if with_dropout:
+            inputs = VariableFilter(
+                bricks=([ali.x_discriminator.layers[0]] +
+                        ali.x_discriminator.layers[2::3] +
+                        ali.z_discriminator.layers[::2] +
+                        ali.joint_discriminator.layers[::2]),
+                roles=[INPUT])(cg.variables)
+            cg = apply_dropout(cg, inputs, 0.2)
+        return Model(cg.outputs)
+
+    model = _create_model(with_dropout=False)
+    with batch_normalization(ali):
+        bn_model = _create_model(with_dropout=True)
+
+    pop_updates = list(
+        set(get_batch_normalization_updates(bn_model, allow_duplicates=True)))
+    bn_updates = [(p, m * 0.05 + p * 0.95) for p, m in pop_updates]
+
+    return model, bn_model, bn_updates
+
+
+def create_main_loop(save_path):
+    model, bn_model, bn_updates = create_models()
+    ali, = bn_model.top_bricks
+    discriminator_loss, generator_loss = bn_model.outputs
+
+    step_rule = Adam(learning_rate=LEARNING_RATE, beta1=BETA1)
+    algorithm = ali_algorithm(discriminator_loss, ali.discriminator_parameters,
+                              step_rule, generator_loss,
+                              ali.generator_parameters, step_rule)
+    algorithm.add_updates(bn_updates)
+    streams = create_celeba_data_streams(BATCH_SIZE, MONITORING_BATCH_SIZE)
+    main_loop_stream, train_monitor_stream, valid_monitor_stream = streams
+    bn_monitored_variables = (
+        [v for v in bn_model.auxiliary_variables if v.name.startswith('ali')] +
+        bn_model.outputs)
+    monitored_variables = (
+        [v for v in model.auxiliary_variables if v.name.startswith('ali')] +
+        model.outputs)
+    extensions = [
+        Timing(),
+        FinishAfter(after_n_epochs=NUM_EPOCHS),
+        DataStreamMonitoring(
+            bn_monitored_variables, train_monitor_stream, prefix="train",
+            updates=bn_updates),
+        DataStreamMonitoring(
+            monitored_variables, valid_monitor_stream, prefix="valid"),
+        Checkpoint(save_path, after_epoch=True, after_training=True,
+                   use_cpickle=True),
+        ProgressBar(),
+        Printing(),
+    ]
+    main_loop = MainLoop(model=bn_model, data_stream=main_loop_stream,
+                         algorithm=algorithm, extensions=extensions)
+    return main_loop
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser(description="Train ALI on CelebA")
+    parser.add_argument("--save-path", type=str, default='ali_celeba.tar',
+                        help="main loop save path")
+    args = parser.parse_args()
+    create_main_loop(args.save_path).run()
---
+++ experiments/semi_supervised_svhn.py
@@ -0,0 +1,93 @@
+import argparse
+
+import numpy
+from fuel.datasets import H5PYDataset
+from fuel.schemes import ShuffledScheme
+from fuel.streams import DataStream
+from sklearn.svm import LinearSVC
+
+
+def main(dataset_path, use_c, log_min, log_max, num_steps):
+    train_set = H5PYDataset(
+        dataset_path, which_sets=('train',), sources=('features', 'targets'),
+        subset=slice(0, 63257), load_in_memory=True)
+    train_stream = DataStream.default_stream(
+        train_set,
+        iteration_scheme=ShuffledScheme(train_set.num_examples, 1000))
+    train_features, train_targets = next(train_stream.get_epoch_iterator())
+
+    valid_set = H5PYDataset(
+        dataset_path, which_sets=('train',), sources=('features', 'targets'),
+        subset=slice(63257, 73257), load_in_memory=True)
+    valid_features, valid_targets = valid_set.data_sources
+
+    test_set = H5PYDataset(
+        dataset_path, which_sets=('test',), sources=('features', 'targets'),
+        load_in_memory=True)
+    test_features, test_targets = test_set.data_sources
+
+    if use_c is None:
+        best_error_rate = 1.0
+        best_C = None
+        for log_C in numpy.linspace(log_min, log_max, num_steps):
+            C = numpy.exp(log_C)
+            svm = LinearSVC(C=C)
+            svm.fit(train_features, train_targets.ravel())
+            error_rate = 1 - numpy.mean(
+                [svm.score(valid_features[1000 * i: 1000 * (i + 1)],
+                           valid_targets[1000 * i: 1000 * (i + 1)].ravel())
+                 for i in range(10)])
+            if error_rate < best_error_rate:
+                best_error_rate = error_rate
+                best_C = C
+            print('C = {}, validation error rate = {} '.format(C, error_rate) +
+                  '(best is {}, {})'.format(best_C, best_error_rate))
+    else:
+        best_C = use_c
+
+    error_rates = []
+    for _ in range(10):
+        train_features, train_targets = next(train_stream.get_epoch_iterator())
+        svm = LinearSVC(C=best_C)
+        svm.fit(train_features, train_targets.ravel())
+        error_rates.append(1 - numpy.mean(
+            [svm.score(valid_features[1000 * i: 1000 * (i + 1)],
+                       valid_targets[1000 * i: 1000 * (i + 1)].ravel())
+             for i in range(10)]))
+
+    print('Validation error rate = {} +- {} '.format(numpy.mean(error_rates),
+                                                     numpy.std(error_rates)))
+
+    error_rates = []
+    for _ in range(100):
+        train_features, train_targets = next(train_stream.get_epoch_iterator())
+        svm = LinearSVC(C=best_C)
+        svm.fit(train_features, train_targets.ravel())
+        s = 1000 * numpy.sum(
+            [svm.score(test_features[1000 * i: 1000 * (i + 1)],
+                       test_targets[1000 * i: 1000 * (i + 1)].ravel())
+             for i in range(26)])
+        s += 32 * svm.score(test_features[-32:], test_targets[-32:].ravel())
+        s = s / 26032.0
+        error_rates.append(1 - s)
+
+    print('Vest error rate = {} +- {} '.format(numpy.mean(error_rates),
+                                               numpy.std(error_rates)))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="ALI-based semi-supervised "
+                                                 "training on SVHN")
+    parser.add_argument("main_loop_path", type=str,
+                        help="path to the saved main loop")
+    parser.add_argument("--use-c", type=float, default=None,
+                        help="evaluate using a specific C value")
+    parser.add_argument("--log-min", type=float, default=-20,
+                        help="minimum C value in log-space")
+    parser.add_argument("--log-max", type=float, default=20,
+                        help="maximum C value in log-space")
+    parser.add_argument("--num-steps", type=int, default=50,
+                        help="number of values to try")
+    args = parser.parse_args()
+    main(args.main_loop_path, args.use_c, args.log_min, args.log_max,
+         args.num_steps)
